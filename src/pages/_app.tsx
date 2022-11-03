59008204d144f5a75fda0534a60fa407b5461fbf
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-11-03 14:25:52 +0900

    gaの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { GATracking } from 'src/components/GaTracking'
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <GATracking trackingId={GA_TRACKING_ID} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
