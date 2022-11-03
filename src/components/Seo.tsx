66e181def3f01146f549a67f541d342e3a6a2f53
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-11-03 15:18:51 +0900

    twitter ogp image

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -25,7 +25,7 @@ const Seo: VFC<MetaData> = ({
   const title = pageTitle ? `${pageTitle} | ${defaultTitle}` : defaultTitle
   const description = pageDescription ? pageDescription : defaultDescription
   const url = pagePath
-  const imgUrl = 'news/portfolio.png'
+  const imgUrl = 'https://youkan.me/news/portfolio.png'
   const imgWidth = pageImgWidth ? pageImgWidth : 1280
   const imgHeight = pageImgHeight ? pageImgHeight : 640
 
@@ -40,8 +40,10 @@ const Seo: VFC<MetaData> = ({
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={imgUrl} />
-      <meta name="twitter:image" content={imgUrl} />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imgUrl} />
       <meta property="og:image:width" content={String(imgWidth)} />
       <meta property="og:image:height" content={String(imgHeight)} />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
