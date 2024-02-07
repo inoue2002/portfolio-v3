ebb458d4f8a934db588e8d10efaf659e18c2ac18
inoue2002 <inoue2002@icloud.com>
2024-02-07 10:47:17 +0900

    feat news data

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,17 +1,15 @@
 import News from 'src/components/News'
+import { useState } from 'react'
 
-import { useEffect, useState } from 'react'
+export async function getServerSideProps() {
+  const apiUrl = process.env.NODE_ENV === 'production' ? 'https://youkan.me/api/news' : 'http://localhost:3000/api/news';
+  const response = await fetch(apiUrl);
+  const data = await response.json()
+  return { props: { initialNews: data.news } }
+}
 
-export default function NewsPage() {
-  const [news, setNews] = useState([])
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch('/api/news')
-      const data = await response.json()
-      setNews(data.news)
-    }
-    fetchUsers()
-  }, [])
+export default function NewsPage({ initialNews }) {
+  const [news, setNews] = useState(initialNews)
 
   return <div>{news ? <News news={news} /> : ''}</div>
 }
