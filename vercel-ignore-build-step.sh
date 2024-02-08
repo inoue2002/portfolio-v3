63e822835dce15e07b367a5db4e8aa4bbf181079
inoue2002 <inoue2002@icloud.com>
2024-02-08 14:01:19 +0900

    chore 複数人版修正

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -4,10 +4,10 @@
 ALLOWED_USERS=("user1" "test" "user3")
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-echo "VERCEL_GIT_AUTHOR_LOGIN: $VERCEL_GIT_AUTHOR_LOGIN"
+echo "VERCEL_GIT_COMMIT_AUTHOR_LOGIN: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
 
 # ユーザーが許可されたリストに含まれているかチェック
-if [[ " ${ALLOWED_USERS[@]} " =~ " ${VERCEL_GIT_AUTHOR_LOGIN} " ]]; then
+if [[ " ${ALLOWED_USERS[@]} " =~ " ${VERCEL_GIT_COMMIT_AUTHOR_LOGIN} " ]]; then
   # 許可されたユーザーが作成したPRの場合、ビルドを進める
   echo "✅ - Build can proceed"
   exit 1;
