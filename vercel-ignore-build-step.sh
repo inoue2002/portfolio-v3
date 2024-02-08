e3ab6a8ad2f59a22784386d0a465dcaba84b209c
inoue2002 <inoue2002@icloud.com>
2024-02-08 14:44:02 +0900

    feat 自分専用にする

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# 複数の許可されたユーザー名を設定
-ALLOWED_USERS=("inoue2002" "yamada2021" "suzuki1999")
+# 特定のユーザー名を設定 ex "inoue2002"
+ALLOWED_USER="inoue2002"
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_AUTHOR_LOGIN: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
 
-# ユーザーが許可されたリストに含まれているか確認
-if [[ " ${ALLOWED_USERS[@]} " =~ " ${VERCEL_GIT_COMMIT_AUTHOR_LOGIN} " ]]; then
-  # 許可されたユーザーが作成したPRの場合、ビルドを進める
+if [[ "$VERCEL_GIT_COMMIT_AUTHOR_LOGIN" == "$ALLOWED_USER" ]] ; then
+  # 特定のユーザーが作成したPRの場合、ビルドを進める
   echo "✅ - Build can proceed"
-  exit 1
+  exit 1;
 else
-  # 許可されていないユーザーが作成したPRの場合、ビルドをキャンセル
+  # それ以外のユーザーが作成したPRの場合、ビルドをキャンセル
   echo "🛑 - Build cancelled"
-  exit 0
+  exit 0;
 fi
