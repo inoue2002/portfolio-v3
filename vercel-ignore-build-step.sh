d53a1f7b53891636cde07da3a19427ca6077ca90
inoue2002 <inoue2002@icloud.com>
2024-02-08 14:04:47 +0900

    fix 複数人

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# 許可されたユーザーのリスト
-ALLOWED_USERS=("user1" "inoue2002" "user3")
+# 複数の許可されたユーザー名を設定
+ALLOWED_USERS=("inoue2002" "yamada2021" "suzuki1999")
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_AUTHOR_LOGIN: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
 
-# ユーザーが許可されたリストに含まれているかチェック
+# ユーザーが許可されたリストに含まれているか確認
 if [[ " ${ALLOWED_USERS[@]} " =~ " ${VERCEL_GIT_COMMIT_AUTHOR_LOGIN} " ]]; then
   # 許可されたユーザーが作成したPRの場合、ビルドを進める
   echo "✅ - Build can proceed"
-  exit 1;
+  exit 1
 else
-  # 許可されていないユーザーが作成したPRの場合、ビルドをキャンセル
+  # それ以外のユーザーが作成したPRの場合、ビルドをキャンセル
   echo "🛑 - Build cancelled"
   exit 0;
 fi
