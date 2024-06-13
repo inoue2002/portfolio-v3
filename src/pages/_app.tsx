0500c973fc8efcdb0a1be4f6b04fc6c5c8e6b0aa
inoue2002 <inoue2002@icloud.com>
2024-06-13 19:19:44 +0900

    fix font load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Header from 'src/components/Header'
 import Seo from 'src/components/Seo'
 
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Head from 'next/head'
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 
@@ -38,6 +39,54 @@ export default function App({ Component, pageProps }: AppProps) {
     >
       <Seo />
       {GA_TRACKING_ID && <GoogleAnalytics gaId={GA_TRACKING_ID} />}
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Rg.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Bd.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Eb.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <style>
+          {`
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Rg.woff2') format('woff2');
+              font-weight: 400;
+              font-style: normal;
+              font-display: swap;
+            }
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Bd.woff2') format('woff2');
+              font-weight: 700;
+              font-style: normal;
+              font-display: swap;
+            }
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Eb.woff2') format('woff2');
+              font-weight: 800;
+              font-style: normal;
+              font-display: swap;
+            }
+          `}
+        </style>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
