d5999b2530e221013cb32467f4c6122cd4d8900a
inoue2002 <inoue2002@icloud.com>
2024-06-13 17:33:03 +0900

    fix lang ja

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { Html } from 'next/document'
 import Head from 'next/head'
 import Footer from 'src/components/Footer'
 import Header from 'src/components/Header'
@@ -11,69 +12,70 @@ const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider
-      theme={extendTheme({
-        fonts: {
-          heading: 'LINESeedJP, sans-serif',
-          body: 'LINESeedJP, sans-serif',
-        },
-        fontWeights: {
-          thin: 100,
-          normal: 400,
-          bold: 700,
-          extraBold: 800,
-        },
-        styles: {
-          global: {
-            '.text-thin': {
-              fontWeight: 100,
-            },
-            '.text-normal': {
-              fontWeight: 400,
-            },
-            '.text-bold': {
-              fontWeight: 700,
-            },
-            '.text-extraBold': {
-              fontWeight: 800,
+    <Html lang="ja">
+      <ChakraProvider
+        theme={extendTheme({
+          fonts: {
+            heading: 'LINESeedJP, sans-serif',
+            body: 'LINESeedJP, sans-serif',
+          },
+          fontWeights: {
+            thin: 100,
+            normal: 400,
+            bold: 700,
+            extraBold: 800,
+          },
+          styles: {
+            global: {
+              '.text-thin': {
+                fontWeight: 100,
+              },
+              '.text-normal': {
+                fontWeight: 400,
+              },
+              '.text-bold': {
+                fontWeight: 700,
+              },
+              '.text-extraBold': {
+                fontWeight: 800,
+              },
             },
           },
-        },
-      })}
-    >
-      <Seo />
-      {GA_TRACKING_ID && <GoogleAnalytics gaId={GA_TRACKING_ID} />}
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Th.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Rg.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Bd.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Eb.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <style>
-          {`
+        })}
+      >
+        <Seo />
+        {GA_TRACKING_ID && <GoogleAnalytics gaId={GA_TRACKING_ID} />}
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/LINESeedJP_OTF_Th.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/LINESeedJP_OTF_Rg.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/LINESeedJP_OTF_Bd.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/LINESeedJP_OTF_Eb.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <style>
+            {`
             @font-face {
               font-family: 'LINESeedJP';
               src: url('/fonts/LINESeedJP_OTF_Th.woff2') format('woff2');
@@ -99,11 +101,12 @@ export default function App({ Component, pageProps }: AppProps) {
               font-style: normal;
             }
           `}
-        </style>
-      </Head>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </ChakraProvider>
+          </style>
+        </Head>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
+    </Html>
   )
 }
