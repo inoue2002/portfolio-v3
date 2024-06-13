cfcaf17998b7455b75997de1135516d8e5979330
inoue2002 <inoue2002@icloud.com>
2024-06-13 17:28:52 +0900

    fix iframe loading

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -47,18 +47,33 @@ export default function ContactPage() {
           </span>
         </div>
 
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLScUfiliW6HuYU2DBAsL10V3K6XOzlaG_PSJuGW2vY9tfguXuw/viewform?embedded=true"
-          width="640"
-          height="1000"
-        >
-          読み込んでいます…
-        </iframe>
-        {/* 
-        <iframe 
-          src="https://form.run/embed/@inoueyosuke--nxukqNdgRidZ6BU675Ux?embed=direct" 
-          style={{ width: '70%', height: '1200px', border: 'none' }}
-        ></iframe> */}
+        <div style={{ position: 'relative', width: '640px', height: '1000px' }}>
+          <div
+            id="loadingMessage"
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              textAlign: 'center',
+              zIndex: 1,
+            }}
+          >
+            読み込んでいます…
+          </div>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLScUfiliW6HuYU2DBAsL10V3K6XOzlaG_PSJuGW2vY9tfguXuw/viewform?embedded=true"
+            width="640"
+            height="1000"
+            style={{ position: 'absolute', top: 0, left: 0 }}
+            onLoad={() => {
+              const loadingMessage = document.getElementById('loadingMessage');
+              if (loadingMessage) {
+                loadingMessage.style.display = 'none';
+              }
+            }}
+          ></iframe>
+        </div>
       </Box>
     </div>
   )
