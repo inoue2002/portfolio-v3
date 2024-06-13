6a0abce445dbd75e2e6dabc730ee02b2a81458ee
inoue2002 <inoue2002@icloud.com>
2024-06-13 19:05:55 +0900

    fix head script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,68 +39,6 @@ export default function App({ Component, pageProps }: AppProps) {
     >
       <Seo />
       {GA_TRACKING_ID && <GoogleAnalytics gaId={GA_TRACKING_ID} />}
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Th.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Rg.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Bd.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/LINESeedJP_OTF_Eb.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <style>
-          {`
-            @font-face {
-              font-family: 'LINESeedJP';
-              src: url('/fonts/LINESeedJP_OTF_Th.woff2') format('woff2');
-              font-weight: 100;
-              font-style: normal;
-              font-display: swap;
-            }
-            @font-face {
-              font-family: 'LINESeedJP';
-              src: url('/fonts/LINESeedJP_OTF_Rg.woff2') format('woff2');
-              font-weight: 400;
-              font-style: normal;
-              font-display: swap;
-            }
-            @font-face {
-              font-family: 'LINESeedJP';
-              src: url('/fonts/LINESeedJP_OTF_Bd.woff2') format('woff2');
-              font-weight: 700;
-              font-style: normal;
-              font-display: swap;
-            }
-            @font-face {
-              font-family: 'LINESeedJP';
-              src: url('/fonts/LINESeedJP_OTF_Eb.woff2') format('woff2');
-              font-weight: 800;
-              font-style: normal;
-              font-display: swap;
-            }
-          `}
-        </style>
-      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,70 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Head, Html, Main, NextScript } from 'next/document'
+
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Th.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Rg.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Bd.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/LINESeedJP_OTF_Eb.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <style>
+          {`
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Th.woff2') format('woff2');
+              font-weight: 100;
+              font-style: normal;
+              font-display: swap;
+            }
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Rg.woff2') format('woff2');
+              font-weight: 400;
+              font-style: normal;
+              font-display: swap;
+            }
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Bd.woff2') format('woff2');
+              font-weight: 700;
+              font-style: normal;
+              font-display: swap;
+            }
+            @font-face {
+              font-family: 'LINESeedJP';
+              src: url('/fonts/LINESeedJP_OTF_Eb.woff2') format('woff2');
+              font-weight: 800;
+              font-style: normal;
+              font-display: swap;
+            }
+          `}
+        </style>
+      </Head>
       <body>
         <Main />
         <NextScript />
