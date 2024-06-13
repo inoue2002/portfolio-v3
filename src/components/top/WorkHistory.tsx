b54837b01f054a4e115c6141864a7d4a82d0f915
inoue2002 <inoue2002@icloud.com>
2024-06-13 19:55:25 +0900

    fix 仕事履歴のデザイン修正

--- src/components/top/WorkHistory.tsx
+++ src/components/top/WorkHistory.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spacer } from '@chakra-ui/react'
+import { Heading, Spacer, Text } from '@chakra-ui/react'
 
 export default function WorkHistory() {
   return (
@@ -7,24 +7,42 @@ export default function WorkHistory() {
         Work History
       </Heading>
       <Spacer marginTop={'10px'} />
-      <Heading as="h3" size="md" marginTop={'5px'}>
-        2020/03~ GONENGOLLC(受託開発・小学生向けプログラミング教育事業)
-      </Heading>
-      <Heading as="h3" size="md" marginTop={'5px'}>
-        2021/05~2021/12 株式会社Civichat(自社開発)
-      </Heading>
-      <Heading as="h3" size="md" marginTop={'5px'}>
-        2021/08~2022/12 on株式会社(自社開発・受託開発)
-      </Heading>
-      <Heading as="h3" size="md" marginTop={'5px'}>
-        2022/02~ 株式会社OH MY GOD(自社開発)
-      </Heading>
-      <Heading as="h3" size="md" marginTop={'5px'}>
-        2022/05~2022/12 dotstudio株式会社(講師)
-      </Heading>
-      <Heading as="h3" size="md" marginTop={'5px'}>
-        2023/05~ 株式会社picon(自社開発)
-      </Heading>
+      <div style={{ marginTop: '5px' }}>
+        <Heading as="h3" size="md">
+          GONENGOLLC
+        </Heading>
+        <Text color="gray.500">2020/03~ (受託開発・小学生向けプログラミング教育事業)</Text>
+      </div>
+      <div style={{ marginTop: '5px' }}>
+        <Heading as="h3" size="md">
+          株式会社Civichat
+        </Heading>
+        <Text color="gray.500">2021/05~2021/12 (自社開発)</Text>
+      </div>
+      <div style={{ marginTop: '5px' }}>
+        <Heading as="h3" size="md">
+          on株式会社
+        </Heading>
+        <Text color="gray.500">2021/08~2022/12 (自社開発・受託開発)</Text>
+      </div>
+      <div style={{ marginTop: '5px' }}>
+        <Heading as="h3" size="md">
+          株式会社OH MY GOD
+        </Heading>
+        <Text color="gray.500">2022/02~ (自社開発)</Text>
+      </div>
+      <div style={{ marginTop: '5px' }}>
+        <Heading as="h3" size="md">
+          dotstudio株式会社
+        </Heading>
+        <Text color="gray.500">2022/05~2022/12 (講師)</Text>
+      </div>
+      <div style={{ marginTop: '5px' }}>
+        <Heading as="h3" size="md">
+          株式会社picon
+        </Heading>
+        <Text color="gray.500">2023/05~ (自社開発)</Text>
+      </div>
     </div>
   )
 }
